24c3cd22fe1e4b52f3476edae7dd03cce87b2178
sad-man-sakib <ss1085045@gmail.com>
2025-06-23 01:36:24 +0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+const cacheName = 'html-app-cache-v1';
+const filesToCache = [
+  './',
+  './index.html',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => cache.addAll(filesToCache))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
